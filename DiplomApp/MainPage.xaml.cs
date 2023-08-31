443cb1e38607985cf4822257d2ec2ed3e1523385
shura <shura@DESKTOP-STVE0IJ>
2023-08-31 09:44:47 +0300

    Добавьте файлы проекта.

---
+++ DiplomApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DiplomApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void ButtonClicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(emailField.Text))
+                errorText.Text = "Не указан логин";
+            else if (string.IsNullOrEmpty(passField.Text))
+                    errorText.Text = "Не указан пароль";
+            else
+            {
+                errorText.Text = "";
+                nextButton.Text = "Вход выполнен";
+                nextButton.TextColor = Color.Green;
+                await Navigation.PushAsync(new TabbedPage1());
+            }
+
+            MessagingCenter.Send<object, string>(this,"EmailEntered", emailField.Text);
+
+        }
+
+    }
+}
---
+++ DiplomApp/TabbedPage1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DiplomApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+
+        public TabbedPage1()
+        {
+            InitializeComponent();
+
+            MessagingCenter.Subscribe<object, string>(this, "EmailEntered", (sender, arg) =>
+            {
+                 email = arg;
+            });
+       
+    }
+
+        public string email { get; private set; }
+
+        private void Butt1_Clicked(object sender, EventArgs e)
+        {
+            switch1.BackgroundColor = Color.Green;
+            switch1.Text = "Объект на охране";
+        }
+        private void Butt2_Clicked(object sender, EventArgs e)
+        {
+            switch1.BackgroundColor = Color.Red;
+            switch1.Text = "Снят с охраны";
+        }
+    }
+}
